4f75a9c411e1b80fe8cb4896a33b037cf24b8ea3
RAHUL NAIK <97820839+naik-r@users.noreply.github.com>
2022-01-25 15:25:45 +0530

    Add files via upload

---
+++ arrayaddress.cpp
@@ -0,0 +1,7 @@
+#include<stdio.h>
+ main() {
+ 	int a[3]={11,22,33};
+ 	printf("\n a[0] VALUE IS %d,ADDRESS IS %u",a[0],&a[0]);
+ 	printf("\n a[1] VALUE IS %d,ADDRESS IS %u",a[1],&a[1]);
+ 	printf("\n a[2] VALUE IS %d,ADDRESS IS %u",a[2],&a[2]);
+ }
---
+++ averagearray.cpp
@@ -0,0 +1,15 @@
+#include<stdio.h>
+ main() {
+ 	float avg,sum=0;
+ 	int marks[30];
+ 	for(int i=0;i<=29;i++) {
+ 	 printf("ENTER THE MARKS:");
+ 	 scanf("%d",&marks);
+ }
+ 	for(int i=0;i<=29;i++) {
+ 		sum=sum+marks[30];
+	 }
+        avg=sum/30;
+	 printf("AVERAGE = %d",avg);	  	  
+ 	
+ }
